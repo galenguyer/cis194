048493dcb5092b46120086b52938f85c3ccbb047
Galen Guyer <galen@galenguyer.com>
2022-02-01 01:46:54 -0500

    Homework 1: use Integer instead of Int

--- src/Hw1.hs
+++ src/Hw1.hs
@@ -1,23 +1,27 @@
 module Hw1
     ( toDigits,
     toDigitsRev,
-    doubleEveryOther
+    doubleEveryOther,
+    sumDigits
     ) where
 
-toDigits :: Int -> [Int]
+toDigits :: Integer -> [Integer]
 toDigits 0 = []
 toDigits n
     | n < 0         = []
     | n < 10        = [n]
     | otherwise     = toDigits (div n 10) ++ toDigits (mod n 10)
 
-toDigitsRev :: Int -> [Int]
+toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = (reverse . toDigits) n
 
-doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:[]) = [x]
 doubleEveryOther (x:y:[]) = (x * 2) : y : []
 doubleEveryOther (x:y:zs)
     | mod (length zs) 2 == 0    = (x * 2) : y : (doubleEveryOther zs)  
     | otherwise                 = x : (y * 2) : (doubleEveryOther zs)
+
+sumDigits :: [Integer] -> Integer
+sumDigits = undefined
